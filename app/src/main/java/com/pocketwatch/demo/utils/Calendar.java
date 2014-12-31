af5afb2247334db0fffebee53da86bc6fbce0378
Bennyam Malavazi <bmalavazi@gmail.com>
2014-12-30 20:17:36 -0800

    UI Changes, Calendar and Duration code

---
+++ app/src/main/java/com/pocketwatch/demo/utils/Calendar.java
@@ -0,0 +1,36 @@
+package com.pocketwatch.demo.utils;
+
+import android.util.Log;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by bmalavazi on 12/30/14.
+ */
+public class Calendar {
+    private static final String TAG = "Calendar";
+    private String mDateString;
+    private static final String mFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+    private SimpleDateFormat mSdf;
+    private Date mDate;
+    private String mFormattedDateString;
+
+    public Calendar(String date) {
+        mDateString = date;
+        mSdf = new SimpleDateFormat(mFormat);
+        try {
+            mDate = mSdf.parse(mDateString);
+            mSdf.applyPattern("EEEE, MMM dd, yyyy hh:mm a");
+            mFormattedDateString = mSdf.format(mDate);
+            Log.d(TAG, mFormattedDateString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getDateString() {
+        return mFormattedDateString;
+    }
+}
